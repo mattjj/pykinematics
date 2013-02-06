391e68e010ccfbbf5be90d1aea56aa9ff1a3f806
Matthew Johnson <mattjj@csail.mit.edu>
2013-02-05 21:10:06 -0500

    tweaked demo parameters

--- viz.py
+++ viz.py
@@ -188,7 +188,7 @@ def constrained_chain_example():
                     ])
             ])
     tree = BigchartJointTreeFKViz(treeroot,bigchart,temp)
-    solver = ik.construct_solver(tree,dampening_factors=2.,
+    solver = ik.construct_solver(tree,dampening_factors=5.,
             prior_variances=np.array([10.,1.,1.,1.,1.]),
             tol=1e-2,maxiter=50,
             jointmins=-np.pi/3,jointmaxes=np.pi/3,
